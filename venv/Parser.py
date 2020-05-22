d9737d02622ac910d87f39f23a6f73612616325e
Sergey <Sergey@SERGEY->
2020-05-22 12:54:18 +0300

    Raw version, multi-page parsing finished, work properly, but only without js ....

--- venv/Parser.py
+++ venv/Parser.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://auto.ria.com/search/?category_id=0&marka_id=62&model_id=585&state%5B0%5D=0&s_yers%5B0%5D=2001&po_yers%5B0%5D=2005&price_ot=&price_do='
+# URL = 'https://auto.ria.com/search/?category_id=0&marka_id=62&model_id=585&state%5B0%5D=0&s_yers%5B0%5D=2001&po_yers%5B0%5D=2005&price_ot=&price_do='
+URL = 'https://auto.ria.com/car/renault/dokker/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
     'accept': '*/*'}
 
@@ -11,9 +12,9 @@ def get_html(url, params=None):
     return r
 
 
-def get_pages_count(html):
+def get_pages_count (html):
     soup = BeautifulSoup(html, 'html.parser')
-    pagination = soup.find_all('span', class_='page-item mhide')
+    pagination = soup.find_all('span', class_='mhide')
     if pagination:
         return int(pagination[-1].get_text())
     else:
@@ -27,10 +28,10 @@ def get_content(html):
     cars = []
     for item in items:
         cars.append({
-            'link': item.find('a', class_='address').get('href'),
-            'title': item.find('a', class_='address').get('title'),
-            'Price USD': item.find('span', class_='bold green size22').get_text(),
-            'Price UAH': item.find('span', class_='i-block').find_next('span').get_text()
+            'Link...': item.find('a', class_='address').get('href'),
+            'Title...': item.find('a', class_='address').get('title'),
+            'Price USD...': item.find('span', class_='bold green size22').get_text(),
+            'Price UAH...': item.find('span', class_='i-block').find_next('span').get_text()
         })
 
     return cars
@@ -46,7 +47,8 @@ def parse():
             html = get_html(URL, params={'page':page})
             cars.extend(get_content(html.text))
             # cars = get_content(html.text)
-        print(cars)
+        # print(cars) # Print list of a cars
+        print(len(cars))
     else:
         print('Error')
 
